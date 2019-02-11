829ba0db2c88f1075c7f848b484b504ba61a33ec
EJACOBS <bU6Am9yrRPBOSPF3aqZR>
2019-02-11 11:25:22 +0100

    first commit

---
+++ DotNetChillflix/Data/DotNetChillflixContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetChillflix.Models
+{
+    public class DotNetChillflixContext : DbContext
+    {
+        public DotNetChillflixContext (DbContextOptions<DotNetChillflixContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DotNetChillflix.Models.Video> Video { get; set; }
+    }
+}
---
+++ DotNetChillflix/Migrations/20190211091029_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetChillflix.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Video",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Thumbnail = table.Column<string>(nullable: true),
+                    Genre = table.Column<string>(nullable: true),
+                    Duration = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Video", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Video");
+        }
+    }
+}
